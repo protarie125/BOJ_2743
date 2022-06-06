a48609f97f1c986df9f6f4fe285046db37bd7cc3
protarie125 <proarie@gmail.com>
2022-06-07 07:33:39 +0900

    초기 커밋.

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	string x;
+	cin >> x;
+
+	cout << x.length();
+
+	return 0;
+}
